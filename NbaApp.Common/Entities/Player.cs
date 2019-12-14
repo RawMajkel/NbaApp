6cdb0e3ce3bbe53c28b484df2d71f4f7f02c1ca3
Michał Droździk <m.drozdzik97@gmail.com>
2019-12-14 14:27:53 +0100

    Adding entities, working on Player

---
+++ NbaApp.Common/Entities/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NbaApp.Common.Entities
+{
+    public class Player
+    {
+        public Guid ID => Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string HomeCity { get; set; }
+        public string HomeCountry { get; set; }
+        public int Age => DateTime.Now.Year - DateOfBirth.Year;
+        public PlayerHeight Height { get; set; }
+        public PlayerWeight Weight { get; set; }
+        public PlayerPosition Position { get; set; }
+        public Team CurrentTeam { get; set; }
+    }
+}
